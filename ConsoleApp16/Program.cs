5271b38b1856a73880d76758f58b0d6540c99c41
Admin <Admin@DESKTOP-UGP0C2G>
2024-12-26 11:19:57 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class PhoneCall
+{
+    private int duration;
+    private decimal costPerMinute;
+    public PhoneCall(int duration, decimal costPerMinute)
+    {
+        this.duration = duration;
+        this.costPerMinute = costPerMinute;
+    }
+    public string GetInfo()
+    {
+        return $"Продолжительность разговора: {duration} мин, Стоимость одной минуты: {costPerMinute} руб.";
+    }
+    public decimal CalculateTotalCost()
+    {
+        return duration * costPerMinute;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите продолжительность разговора в минутах:");
+        string durationInput = Console.ReadLine();
+        Console.WriteLine("Введите стоимость одной минуты :");
+        string costInput = Console.ReadLine();
+        if (int.TryParse(durationInput, out int duration) &&
+            decimal.TryParse(costInput, out decimal cost))
+        {
+            PhoneCall call = new PhoneCall(duration, cost);
+            decimal totalCost = call.CalculateTotalCost();
+            Console.WriteLine(call.GetInfo());
+            Console.WriteLine($"Общая стоимость разговора: {totalCost} руб.");
+        }
+        else
+        {
+            Console.WriteLine("Введите корректные значения.");
+        }
+    }
+}
